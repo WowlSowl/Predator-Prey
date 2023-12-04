cf3cd16b49142736d8230a516c6a0b0ac1200339
Andrew Hirt <142833305+WowlSowl@users.noreply.github.com>
2023-12-04 19:07:59 +0000

    beginning

---
+++ doodleBug.h
@@ -0,0 +1,19 @@
+#ifndef DOODLEBUG_H
+#define DOODLEBUG_H
+
+#include "organism_h"
+#include <iostream>
+#include <string>
+
+class doodBug{
+    private:
+    const ID&;
+
+    public:
+    explicit doodBug(ID&);
+
+}
+
+
+
+#endif
---
+++ organism.h
@@ -0,0 +1,14 @@
+#ifndef ORGANISM_H
+#define ORGANISM_H
+
+class Organism{
+
+
+public:
+    Organism();
+
+void move();
+void starve();
+void breed();
+};
+#endif
